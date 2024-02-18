facd89e720ef71b29b9206ae367f5e5fa9eeacc6
Ali Amirnezhad <webilix@gmail.com>
2024-02-18 03:40:56 +0330

    Use write stream for csv export

--- src/export/providers/csv.service.ts
+++ src/export/providers/csv.service.ts
@@ -1,35 +1,47 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 
-import { IExportConfig, IExportTable } from '../export.interface';
+import { IExportTable } from '../export.interface';
 
 @Injectable()
 export class ExportCsvService {
-    constructor(@Inject('EXPORT_CONFIG') private readonly config: IExportConfig) {}
+    constructor() {}
+
+    private getJSON = (line: any[]) =>
+        JSON.stringify(line.reverse())
+            .replace(/(^\[)|(\]$)/gm, '')
+            .replace(/""/g, '')
+            .replace(/\\"/g, '""')
+            .replace(/\\n/g, '\r\n');
 
     export(path: string, table: IExportTable): Promise<void> {
         return new Promise<void>((resolve, reject) => {
             try {
-                const content: any[][] = [table.columns.map((column) => column.title)];
-                table.rows.forEach((row) => {
-                    const data: any[] = [];
+                const stream = fs.createWriteStream(path, { flags: 'w' });
+
+                const line: string[] = table.columns.map((column) => column.title);
+                stream.write(this.getJSON(line), (error) => writeLine(error, 0));
+
+                const writeLine = (error: any, index: number): void => {
+                    if (!!error) {
+                        stream.close();
+                        reject();
+                        return;
+                    }
+
+                    if (!table.rows[index]) {
+                        stream.close();
+                        resolve();
+                        return;
+                    }
+
+                    const line: any[] = [];
                     table.columns.forEach((column) => {
-                        const value = row[column.id];
-                        data.push(value === undefined ? '' : value);
+                        const value = table.rows[index][column.id];
+                        line.push(value === undefined ? '' : value);
                     });
-                    content.push(data);
-                });
-
-                const lines: string[] = content.map((c) =>
-                    JSON.stringify(c.reverse())
-                        .replace(/(^\[)|(\]$)/gm, '')
-                        .replace(/""/g, '')
-                        .replace(/\\"/g, '""')
-                        .replace(/\\n/g, '\r\n'),
-                );
-
-                fs.writeFileSync(path, lines.join('\n'));
-                resolve();
+                    stream.write('\n' + this.getJSON(line), (error) => writeLine(error, index + 1));
+                };
             } catch (e) {
                 reject();
             }
